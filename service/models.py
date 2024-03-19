132839568dd926db7f8a3f752ac8bbc85e06ae7a
Karan <karan.dhanwani77@gmail.com>
2024-03-19 15:10:55 -0400

    created the service category

--- service/models.py
+++ service/models.py
@@ -7,3 +7,11 @@ def service_path(instance, filename):
         instance.id,
         filename
     )
+
+class ServiceCategory(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.id
