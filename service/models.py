6fe2c04aa9a732d30d4376464b8096915c07b66d
Karan <karan.dhanwani77@gmail.com>
2024-03-19 15:20:18 -0400

    created the service booking model

--- service/models.py
+++ service/models.py
@@ -2,11 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 from user.models import *
 
+
 def service_path(instance, filename):
     return 'service/{}/{}'.format(
         instance.id,
         filename
     )
+
 
 class ServiceCategory(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
@@ -15,6 +17,7 @@ class ServiceCategory(models.Model):
 
     def __str__(self):
         return self.id
+
 
 class ProviderService(models.Model):
     title = models.CharField(max_length=255, null=False, blank=False)
@@ -31,6 +34,7 @@ class ProviderService(models.Model):
     def __str__(self):
         return str(self.id)
 
+
 class ProviderAvailability(models.Model):
     day = models.CharField(max_length=20)
     available = models.BooleanField(default=False)
@@ -42,3 +46,24 @@ class ProviderAvailability(models.Model):
 
     def _str_(self):
         return f"{self.user.username}'s availability on {self.day}"
+
+class ServiceBooking(models.Model):
+    BOOKING_STATUS = (
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('cancelledbycustomer', 'CancelledByCustomer'),
+        ('cancelledbyprovider', 'CancelledByProvider')
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service = models.ForeignKey(ProviderService, on_delete=models.CASCADE, null=False, blank=False)
+    desc = models.TextField(null=True, blank=True)
+    status = models.CharField(choices = BOOKING_STATUS, default = "pending", max_length=50, null=False, blank=False)
+    appointment_time = models.CharField(max_length=128, null=False, blank=False)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, null=False, blank=False)
+    price = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.id
